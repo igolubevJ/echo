43cbbf553e531c65051b9705a113da37e00c283e
igolubev <igolubevapp@gmail.com>
2024-02-24 09:48:39 +0300

    Add moduledoc

--- lib/echo.ex
+++ lib/echo.ex
@@ -1,4 +1,29 @@
 defmodule Echo do
+  @moduledoc """
+  This model creates an echo process.
+  You can create multiple echo processes in your application.
+
+  To create an echo process, simply call the start function and pass the pid
+  of the process that will receive responses from the echo process.
+  To interact with this process, you can use the `Echo.ping/1` function or send
+  a message using the `Kernel.send/2`.
+
+  #### Examples
+      iex> {:ok, pid} = Echo.start(self())
+      {:ok, pid}
+      iex> Echo.ping(pid)
+      {:pong, :nonode@nohost}
+
+  If you send an erroneous message, Echo return error tuple `{:error}`.
+
+  #### Examples
+      iex> {:ok, pid} = Echo.start(self())
+      {:ok, pid}
+      iex> send(pid, :some)
+      :some
+      iex> receive do msg -> msg end
+      {:error}
+  """
   @spec start(pid()) :: {:ok, pid()}
   def start(from) do
     pid = spawn(fn ->
